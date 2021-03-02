04b8d6b899e4d6437366841df6dba998181fbb5d
Damir Bakty <damir050602@gmail.com>
2021-03-02 21:13:46 +0600

    hello
    Signed-off-by: Damir Bakty <damir050602@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,22 @@
 package com.company;
 
+
 import javax.swing.*;
-import java.sql.*;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        // Create and set up a frame window
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                MyGUIform myGUIform = new MyGUIform();
+                MainMenu myGUIform = new MainMenu();
                 myGUIform.setVisible(true);
             }
         });
 
-        /*Scanner in = new Scanner(System.in);
-        Service service = new Service();
-        Products shopping = new Products();
 
-        System.out.println("Enter name and phone number: ");
-
-        service.order();
-        shopping.start();
-        service.seeOrder();*/
     }
+
 }
--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -3,21 +3,27 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MyGUIform extends JFrame {
+public class MainMenu extends JFrame {
     private JButton Kitchen;
     private JPanel rootPanel;
-    private JTextArea textArea1;
+    private JButton kitchenButton;
+    private JButton barButton;
+    private JButton confectioneryButton;
 
-    public MyGUIform(){
-
+    public MainMenu(){
         add(rootPanel);
         setTitle("My First GUI Form");
         setSize(400,500);
-        Kitchen.addActionListener(new ActionListener() {
+        kitchenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPane, "Hellfdsfo world");
+                
+                setVisible(false);
+                Kitchen kitchen = new Kitchen();
+                kitchen.setVisible(true);
             }
         });
     }
+
+
 }
